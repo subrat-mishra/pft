0530625ee16e6b53c0ba431a0f678fb74c9dd5d7
subrat.mishra <subrat.mishra@yahoo.com>
2019-05-05 16:57:24 +0530

    Improved performance of the file transfer.

--- src/main/java/com/apidata/pft/PFTChunkClient.java
+++ src/main/java/com/apidata/pft/PFTChunkClient.java
@@ -12,7 +12,7 @@ import java.nio.channels.FileChannel;
 import java.nio.channels.SocketChannel;
 import java.util.concurrent.Callable;
 
-import static com.apidata.pft.PFTConstants.BUFFER_SIZE;
+import static com.apidata.pft.PFTConstants.LENGTH_SIZE;
 
 /**
  * PFTChunkClient thread pulls data for specific offset from server and returns a Result object.
@@ -50,46 +50,53 @@ public class PFTChunkClient implements Callable {
         try {
             client = SocketChannel.open(hostAddress);
             LOG.info("Connect to server:{}", client.getRemoteAddress());
+            //            client.socket().setReceiveBufferSize(LENGTH_SIZE);
 
             if (client.isOpen()) {
                 // formulate the request message and send it to the server.
 
-                long bufferChunks = offset / BUFFER_SIZE;
-                long remainder = offset % BUFFER_SIZE;
+                long bufferChunks = offset / LENGTH_SIZE;
+                long remainder = offset % LENGTH_SIZE;
 
                 FileChunkRequestMsg msg = null;
                 int len = 0;
                 long totalBytes = 0;
                 while (bufferChunks > 0) {
-                    msg = new FileChunkRequestMsg(serverFilePath, startPosition, chunkId, BUFFER_SIZE);
+                    msg = new FileChunkRequestMsg(serverFilePath, startPosition, chunkId,
+                                    LENGTH_SIZE);
                     Message.sendMessage(client, msg);
-                    ByteBuffer buffer = ByteBuffer.allocate(BUFFER_SIZE);
-                    if ((len = client.read(buffer)) > 0) {
+                    ByteBuffer buffer = ByteBuffer.allocate(LENGTH_SIZE);
+                    int currLen = 0;
+                    while (currLen < LENGTH_SIZE && (len = client.read(buffer)) > 0) {
                         totalBytes += len;
                         // put the data into the fileChannel
                         buffer.flip();
                         channel.write(buffer, startPosition);
                         startPosition += len;
                         buffer.clear();
+                        currLen += len;
                     }
                     bufferChunks--;
                 }
 
                 if (remainder > 0) {
-                    msg = new FileChunkRequestMsg(serverFilePath, startPosition, chunkId, (int) remainder);
+                    msg = new FileChunkRequestMsg(serverFilePath, startPosition, chunkId,
+                                    (int) remainder);
                     Message.sendMessage(client, msg);
+                    int currLen = 0;
                     ByteBuffer buffer = ByteBuffer.allocate((int) remainder);
-                    if ((len = client.read(buffer)) > 0) {
+                    while (currLen < remainder && (len = client.read(buffer)) > 0) {
                         totalBytes += len;
                         // put the data into the fileChannel
                         buffer.flip();
                         channel.write(buffer, startPosition);
                         buffer.clear();
+                        currLen += len;
                     }
                 }
                 status = true;
-                LOG.info("Total bytes asked {} downloaded {} by PFTChunkClient-{}", offset, totalBytes,
-                        chunkId);
+                LOG.info("Total bytes asked {} downloaded {} by PFTChunkClient-{}", offset,
+                        totalBytes, chunkId);
             }
         } catch (IOException e) {
             LOG.error("IOException occurred", e);
--- src/main/java/com/apidata/pft/PFTConstants.java
+++ src/main/java/com/apidata/pft/PFTConstants.java
@@ -1,8 +1,7 @@
 package com.apidata.pft;
 
 public class PFTConstants {
-    public static final String PART = ".part";
     public static final int BUFFER_SIZE = 2048;
-    public static final String END_MESSAGE_MARKER = "DONE";
+    public static final int LENGTH_SIZE = 20480;
     public static final int MAX_BUFFER_PER_THREAD = 9998336;
 }
